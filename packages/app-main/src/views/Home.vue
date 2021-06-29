ee46122046b2a5997d0a5acca280bb8d9721fc76
Vision Hung <scps950613@gmail.com>
2021-06-29 19:06:04 +0800

    app-main add global-ui button

--- packages/app-main/src/views/Home.vue
+++ packages/app-main/src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <br>
+    <v-button label="New VButton" icon="mdi-light:home"></v-button>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import { VButton } from 'global-ui';
 import HelloWorld from '../components/HelloWorld.vue';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+    VButton,
   },
 };
 </script>
