5ff0f5009bb9d88fc98ff9940ee0afb440db7729
Vision Hung <scps950613@gmail.com>
2021-06-30 17:05:31 +0800

    add nohist-page and import global-ui

---
+++ packages/nohist-page/src/views/Template.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Template page:
+    <v-button label="New VButton" icon="mdi-account-circle"></v-button>
+  </div>
+</template>
+
+<script>
+import { VButton } from '@vision/global-ui';
+
+export default {
+  components: {
+    VButton
+  }
+}
+</script>
+
+<style>
+
+</style>
