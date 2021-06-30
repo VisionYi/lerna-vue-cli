0a05e7bcd635401ea3fc7aa286cdb8f9b363071e
Vision Hung <scps950613@gmail.com>
2021-06-30 19:29:30 +0800

    change router config

--- packages/nohist-page/src/router/routes.js
+++ packages/nohist-page/src/router/routes.js
@@ -1,6 +1,6 @@
 // uses the import function to dynamically import a Vue component.
 function loadView(view) {
-  return () => import(/* webpackChunkName: "view-[request]" */ `@/views/${view}.vue`);
+  return () => import(/* webpackChunkName: "view-[request]" */ `../views/${view}.vue`);
 }
 
 const routes = [
